2264448b37ea70df14bcbf693408b0ec58d22b04
frontendravi <rsdesign95@gmail.com>
2024-09-07 17:02:17 +0530

    design
    need to pull

---
+++ utils/constants.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    title: "GBRTEK",
+    overview: "The project involved creating a modern, user-friendly interface that effectively communicates the brand’s focus on innovation and technological solutions.",
+    stack: ["Html", "CSS", "BOOTSTRAP", "KEYFRAMES"],
+    link: "https://www.gbrtek.com/",
+    repo: null,
+    isSiteLive: true,
+  },
+  {
+    title: "THE DPS CO",
+    overview:
+      " I utilized the latest web design trends to ensure a responsive, visually appealing, and seamless user experience.",
+    stack: ["Html", "CSS", "GSAP"],
+    link: "https://thedps.co/",
+    repo: "",
+    isSiteLive: true,
+  },
+  {
+    title: "Sidhu Beauty Salon",
+    overview:
+      "A single-page application that helps filmmakers learn more about who their film might be suitable for.",
+    stack: ["Html", "CSS", "Bootstrap"],
+    link: "http://www.sidhubeautysalon.com/",
+    isSiteLive: true,
+  },
+  {
+    title: "KNESTA",
+    overview:
+      "KNESTA Architects is an award-winning architectural firm that specializes in creating bespoke designs that blend functionality with aesthetics.",
+    stack: ["Webflow", "CSS", "Animation"],
+    link: "https://www.knesta.com/",
+    isSiteLive: true,
+  },
+  {
+    title: "Total Solar",
+    overview:
+      "I designed and developed the website for Total Solar, a company specializing in photovoltaic electric systems.",
+    stack: ["WORDPRESS", "CSS", "GSAP"],
+    link: "https://totalsolar.com.au/",
+    repo: null,
+    isSiteLive: true,
+  },
+  {
+    title: "Turko Persian",
+    overview:
+      "I designed the website for Turk Persian Rug Cleaners, focusing on creating a clean, elegant, and user-friendly interface. ",
+    stack: ["HTML", "CSS3"],
+    link: "https://www.turkopersianrugcleaners.com/",
+    // repo: "https://github.com/danielcranney/GPS-Embroidery",
+    isSiteLive: true,
+  },
+];
